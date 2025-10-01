ef83fe00f42fe326e72f5fa455006693f12f03e2
Nafiz <nafizmahmud102@gmail.com>
2025-10-01 22:34:42 +0600

    change in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,8 @@
 export default function Footer() {
+  const year = new Date().getFullYear();
   return (
     <footer className="text-[1.5rem] lg:text-[3rem] py-[1rem] text-center animate-pulse text-white font-bold bg-gray-900">
-      Nafiz M. | 2024 | &copy;
+      &copy; Nafiz Mahmud | {year}
     </footer>
   );
 }
